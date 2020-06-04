9ad83c12764aa8f6df496af84eae4861a81ad6da
Evgeny Tunnikov <tunnikov2013@yandex.ru>
2020-06-04 11:54:43 +0300

    первый коммит первого задания

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let money;
+
+let income;
+
+let message = 'Hello!';
+
+let addExpenses;
+
+let deposit;
+
+let mission;
+
+let period;
+
+alert ('любой текст');
+
+console.log (' Задание простое, но  из-за jshitn все время сбиваюсь, потому что думаю что ошибки уже сделаны ');
